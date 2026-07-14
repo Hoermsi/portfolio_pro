3a509fc9a390686af9d6819e30bf555bb43a728d
Rene Hermann <rene.hermann@outlook.at>
2026-07-14 22:28:29 +0200

    Version auf 1.2.1 erhoehen

--- core/version.py
+++ core/version.py
@@ -3,4 +3,4 @@
 Wird vom In-App-Updater (Vergleich mit dem neuesten GitHub-Release) und vom
 Installer (AppVersion) gemeinsam genutzt. Format: Semantic Versioning "MAJOR.MINOR.PATCH".
 """
-APP_VERSION = "1.2.0"
+APP_VERSION = "1.2.1"
